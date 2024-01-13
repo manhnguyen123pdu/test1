2fb802b16a14edca9a3ef89913427d0840442dd5
Á <manhnguyen.tla@gmail.com>
2024-01-13 19:51:01 +0700

    a

---
+++ app.js
@@ -0,0 +1,15 @@
+// 1.F    2.T   3.T   4.{foo: 1, bar: 2}   5.'The quick brown fox jumps over the lazy dog.’
+
+// 6.undefined   7.[4, 5, 6, 1, 2, 3]    8.[1, 23, 4]    9. 3   10. a = 3, b = 12
+
+// 11.{name: “MindX”}     12.[2, 2, 6, 4, 10]           13. 16
+// // EX1
+// let input = prompt("Nhập string");
+// let splitInput = input.split("");
+// let reverseInput = splitInput.reverse();
+// let output = reverseInput.join("");
+// alert(output);
+// // EX2
+// let input = [1, 2, 3, 5, 4, 2, 6, 4];
+// let output = [...new Set(input)];
+// alert(output);
